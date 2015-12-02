b46ac426e1bd68baa862fe927f23c99396abad08
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2015-12-02 16:54:47 +0000

    #24 changed build code. Now includes amp html creation and uses remarkable

--- build.js
+++ build.js
@@ -2,7 +2,7 @@
 var fs = require('fs');
 var util = require('util');
 var marked = require('marked'); // parse markdown into html
-var h = require('./lib/helpers.js');
+var getPosts = require('./lib/helpers.js').getPosts;
 
 // include header.html file if one exists
 var header = fs.readFileSync(__dirname + '/header.html', 'utf8');
@@ -10,23 +10,25 @@ var header = fs.readFileSync(__dirname + '/header.html', 'utf8');
 var footer = fs.readFileSync(__dirname + '/footer.html', 'utf8');
 var html = header;
 
-h.getPosts(function(err, posts){
+getPosts(function(err, posts){
   // console.log(util.inspect(posts));
   var countdown = posts.length;
   posts.map(function(post){
-    var full = header + marked(post.full) + footer;
+    var full = post.html;
+    // var full = buildAmpPost(post);
+    // var full = header + marked(post.full) + footer;
     fs.writeFile(__dirname+'/posts/'+post.slug +'.html', full, function(err){
       console.log('done');
     });
-    html = html + titleLink(post) + marked(post.intro);
-    if(--countdown === 0){
-      html = html + footer;
-      // summary view:
-      fs.writeFile(__dirname+'/index.html', html, function(err){
-        console.log('done');
-      });
-
-    }
+    // html = html + titleLink(post) + marked(post.intro);
+    // if(--countdown === 0){
+    //   html = html + footer;
+    //   // summary view:
+    //   fs.writeFile(__dirname+'/index.html', html, function(err){
+    //     console.log('done');
+    //   });
+    //
+    // }
   })
 });
 
