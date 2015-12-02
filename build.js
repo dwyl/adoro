5c05c8e77a2b469aa703c74bb5b578224f79e76f
Marie <marie.kasai@live.co.uk>
2015-12-02 12:06:08 +0000

    familiarising with the code

--- build.js
+++ build.js
@@ -14,18 +14,27 @@ h.getPosts(function(err, posts){
   // console.log(util.inspect(posts));
   var countdown = posts.length;
   posts.map(function(post){
+
+    // post.full = post content. Puts the data from the post in between the two parts of index.html
     var full = header + marked(post.full) + footer;
+
+    // write the post's contents into a new .html file.
     fs.writeFile(__dirname+'/posts/'+post.slug +'.html', full, function(err){
-      console.log('done');
+      // console.log(__dirname+'/posts/'+post.slug +'.html');
+      // console.log('done converting .md post in the .html');
     });
+
+    // html = first part of index.html + <h1> tags for the newly created post + first few lines of post
     html = html + titleLink(post) + marked(post.intro);
+
     if(--countdown === 0){
+      // add footer, i.e. the ending body and html tags.
       html = html + footer;
-      // summary view:
+
+      // add in the html file into index.html
       fs.writeFile(__dirname+'/index.html', html, function(err){
         console.log('done');
       });
-
     }
   })
 });
---
+++ posts/processingMdToHTML.md
@@ -0,0 +1,9 @@
+considering the most simple case: md with plain text only.
+
+process:
+
+- read the contents of the .md file.
+- stringify it
+- assumptions:
+  - first line = title --> remove hashtag
+  - 
