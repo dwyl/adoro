b70b03e27aba0780e23fc55e3aef125e1067f804
tormod17 <tormodsmith@gmail.com>
2015-12-03 18:26:18 +0000

    added conversion to md syntax #70

--- public/editor.js
+++ public/editor.js
@@ -2,9 +2,21 @@
 
 document.getElementById('submit').addEventListener('click',function() { 
 	console.log('BOOM');
+	var form = document.getElementById('myForm').value;
+	console.log('FORM',form);
 	var title=  document.getElementById('title').value;
 	var text=  document.getElementById('textBox').value;
 	var imgUrl =  document.getElementById('imgUrl').value;
-	console.log(title , text , imgUrl);
-	return title + text + imgUrl;
+	console.log(addMdHeading(title, "###") +text + addMdImgUrl('image', imgUrl));
+	return addMdHeading(title, "###") + '\\n'+ text + '\\n'+ addMdImgUrl('image', imgUrl);
+
 });
+
+
+function addMdHeading(title,hashes){
+	return hashes + title; 
+}
+
+function addMdImgUrl(description, url){
+   return   '!['+ description+']('+ url+')';	
+}
