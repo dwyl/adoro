3faaadffb873e5f8233c570b71165696f96ec7f9
Conor C <Conorc1000@gmail.com>
2015-12-03 18:31:11 +0000

    added server #70

--- public/editor.js
+++ public/editor.js
@@ -1,10 +1,15 @@
-
-
-document.getElementById('submit').addEventListener('click',function() { 
+document.getElementById('submit').addEventListener('click',function() {
 	console.log('BOOM');
 	var title=  document.getElementById('title').value;
 	var text=  document.getElementById('textBox').value;
 	var imgUrl =  document.getElementById('imgUrl').value;
-	console.log(title , text , imgUrl);
-	return title + text + imgUrl;
+	var all = title+ text+ imgUrl;
+
+	var request = new XMLHttpRequest();
+
+  request.onreadystatechange = function() {
+    console.log(request.responseText);
+  };
+  request.open('POST', "/all" + all ,true);
+  request.send();
 });
